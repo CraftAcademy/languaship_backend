d9fbaa8d6f9cd7343178cb754846dfab0e6d366e
Aiden_Jubelin <aiden.jubelin@gmail.com>
2018-04-13 14:47:44 +0200

    change location creation to factory bot in test env added images to serialization fix test format

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,6 +4,7 @@ FactoryBot.define do
     password_confirmation "password1"
     age 14
     gender "Male"
+    image "http://cdn2.ubergizmo.com/wp-content/uploads/2016/02/homer-simpson.jpeg"
 
     sequence(:name) { |n| "user#{n}" }
     sequence(:email) { |n| "email#{n}@email.com" }
