b500746736559605919d70bb550c2c780e12e7e0
magnus-thor <magnus.thor.ar@gmail.com>
2018-04-14 10:32:43 +0200

    update the seed file to create  all languages

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,48 +8,46 @@
 # user.save
 #
 
-  Location.destroy_all
-  Language.destroy_all
-  User.destroy_all
+Location.destroy_all
+Language.destroy_all
+User.destroy_all
 
-#Languages
-native_languages = [["SWEDISH", true, false], ["FRENCH", true, false],
-                    ["ITALIAN", true, false], ["GERMAN", true, false],
-                    ["ENGLISH", true, false], ["SPANISH", true, false],
-                    ["FINNISH", true, false]]
-learn_languages = [["SWEDISH", false, true], ["FRENCH", false, true],
-                    ["ITALIAN", false, true], ["GERMAN", false, true],
-                    ["ENGLISH", false, true], ["SPANISH", false, true],
-                    ["FINNISH", false, true]]
+# Languages
+languages = [['SWEDISH', true, false], ['FRENCH', true, false],
+             ['ITALIAN', true, false], ['GERMAN', true, false],
+             ['ENGLISH', true, false], ['SPANISH', true, false],
+             ['FINNISH', true, false], ['SWEDISH', false, true],
+             ['FRENCH', false, true], ['ITALIAN', false, true],
+              ['GERMAN', false, true], ['ENGLISH', false, true],
+              ['SPANISH', false, true], ['FINNISH', false, true]]
 
-#Locations
-locations = ["Springfield", "Paris", "Amsterdam"]
+languages.each do |lan|
+  Language.create(name: lan[0], native: lan[1], learn: lan[2])
+end
+# Locations
 
-#User
-name = ["Homer Simpson", "Marge Simpson", "Philip J. Fry"]
-email = ["Homer@simpson.com", "Marge@simpson.com", "Philip@fry.com"]
+locations = %w[Springfield Paris Amsterdam]
+# User
+name = ['Homer Simpson', 'Marge Simpson', 'Philip J. Fry']
+email = ['Homer@simpson.com', 'Marge@simpson.com', 'Philip@fry.com']
 age = [39, 36, 25]
-gender = ["Male", "Female", "Male"]
-images = ["https://cdn1.thr.com/sites/default/files/imagecache/scale_crop_768_433/2016/02/simp2006_homerarmscrossed_f_hires2_-_h_2016.jpg",
-          "https://upload.wikimedia.org/wikipedia/en/0/0b/Marge_Simpson.png",
-          "https://theinfosphere.org/images/0/0d/Fry_promo_2.jpg"]
-
+gender = %w[Male Female Male]
+images = ['https://cdn1.thr.com/sites/default/files/imagecache/scale_crop_768_433/2016/02/simp2006_homerarmscrossed_f_hires2_-_h_2016.jpg',
+          'https://upload.wikimedia.org/wikipedia/en/0/0b/Marge_Simpson.png',
+          'https://theinfosphere.org/images/0/0d/Fry_promo_2.jpg']
 
 3.times do |index|
   user = User.create(
-      name: name[index],
-      age: age[index],
-      gender: gender[index],
-      email: email[index],
-      image: images[index],
-      password: 'password'
+    name: name[index],
+    age: age[index],
+    gender: gender[index],
+    email: email[index],
+    image: images[index],
+    password: 'password'
   )
+
   Location.create(locale: locations[index], user: user)
-  user.languages << Language.create(name: native_languages[index][0],
-                                    native: native_languages[index][1],
-                                    learn: native_languages[index][2])
-  user.languages << Language.create(name: learn_languages[index][0],
-                                    native: learn_languages[index][1],
-                                    learn: learn_languages[index][2])
+  user.languages << Language.find_by(id: Language.first.id + index)
+  user.languages << Language.find_by(id: Language.first.id + index + 8)
   user.save
 end
